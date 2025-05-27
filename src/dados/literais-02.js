2fa02fd28772bdbf7123df676fd9d780519b6caf
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-05-27 15:18:51 -0400

    Adding new codes

--- src/dados/literais-02.js
+++ src/dados/literais-02.js
@@ -1,4 +1,4 @@
-console.log(123); 
-console.log(15.5); 
-console.log('Olá, mundo!'); 
-console.log(true); 
+console.log(123); // number
+console.log(15.5); // number
+console.log('Olá, mundo!');  // string
+console.log(true); // boolean
---
+++ src/dados/variaveis-01.js
@@ -0,0 +1,9 @@
+let x = 10; 
+let y = 20;
+
+console.log(x + y); // 30
+
+x = x + 10; // 10 + 10 = 20
+console.log(x + y); // 20 + 20 = 40
+
+console.log(x, y); // 20, 20
