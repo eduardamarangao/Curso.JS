0a8514dd00040d824b0d687ca964e647069ba05a
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-05-28 20:21:21 -0400

    Adding new codes

--- src/dados/interpolacao.js
+++ src/dados/interpolacao.js
@@ -1,6 +1,6 @@
-const nome = 'Lucas';
-const idade = 25;
-const cidade = 'São Paulo';
+const nome = 'eduarda';
+const idade = 14;
+const cidade = 'rondonópolis';
 
 // const frase = 'Olá! Meu nome é ' + nome + ', tenho ' + idade + 'anos e moro em ' + cidade + '.';
 const frase = `Olá! Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`;
