69bf615e7613aa714839734c2522ed00d698c567
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-05-28 20:22:42 -0400

    Adding new codes

--- src/dados/variaveis-02.js
+++ src/dados/variaveis-02.js
@@ -1,9 +1,9 @@
 const x = 10;
 const y = 20;
 
-const resultadoDaSoma = x + y;
+const resultadoDaSoma = x + y; // 10 + 20 = 30
 
-console.log(resultadoDaSoma);
+console.log(resultadoDaSoma); 
 
-const xAlterado = x + 10;
-console.log(x, xAlterado, y);
+const xAlterado = x + 10; // 10 + 10 = 20
+console.log(x, xAlterado, y); 
