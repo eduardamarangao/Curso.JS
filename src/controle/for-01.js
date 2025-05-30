13d7de76808e280703c08b0d109d63018133191b
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-05-30 15:41:17 -0400

    Adding new codes

---
+++ src/controle/for-01.js
@@ -0,0 +1,10 @@
+/*for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+*/
+
+for (let i = 1; i <= 6; i = i + 1) {
+    console.log(i);
+}
+
+console.log('Fim');
---
+++ src/controle/while-01.js
@@ -0,0 +1,7 @@
+let numero = 1;
+
+while (numero <= 5){
+    console.log("Código foi executado!");
+    numero++;
+}
+    
